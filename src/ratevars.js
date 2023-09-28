6f682f95954b7e2bfd954006c92759bb8235a82f
Brian Rivet <brian@odysseydesignstudio.com>
2023-09-28 10:12:15 -0500

    working on getting plugin ready for use with Craft

--- src/ratevars.js
+++ src/ratevars.js
@@ -1,40 +1,207 @@
-import { Plugin } from 'ckeditor5/src/core';
-import { ButtonView } from 'ckeditor5/src/ui';
+//import { Plugin } from 'ckeditor5/src/core';
+//import { ButtonView } from 'ckeditor5/src/ui';
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { List } from '@ckeditor/ckeditor5-list';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { Command, Plugin } from '@ckeditor/ckeditor5-core';
+import { Widget, toWidget, viewToModelPositionOutsideModelElement } from '@ckeditor/ckeditor5-widget';
+import { Model, addListToDropdown, createDropdown } from '@ckeditor/ckeditor5-ui';
+import { Collection } from '@ckeditor/ckeditor5-utils';
 
 import ckeditor5Icon from '../theme/icons/ckeditor.svg';
 
 export default class Ratevars extends Plugin {
-	static get pluginName() {
-		return 'Ratevars';
-	}
-
-	init() {
-		const editor = this.editor;
-		const t = editor.t;
-		const model = editor.model;
-
-		// Add the "ratevarsButton" to feature components.
-		editor.ui.componentFactory.add( 'ratevarsButton', locale => {
-			const view = new ButtonView( locale );
-
-			view.set( {
-				label: t( 'Ratevars' ),
-				icon: ckeditor5Icon,
-				tooltip: true
-			} );
-
-			// Insert a text into the editor after clicking the button.
-			this.listenTo( view, 'execute', () => {
-				model.change( writer => {
-					const textNode = writer.createText( 'Hello CKEditor 5!' );
-
-					model.insertContent( textNode );
-				} );
-
-				editor.editing.view.focus();
-			} );
-
-			return view;
-		} );
-	}
+    static get requires() {
+        return [ PlaceholderEditing, PlaceholderUI ];
+    }    
 }
+
+class PlaceholderCommand extends Command {
+    execute( { value } ) {
+        const editor = this.editor;
+        const selection = editor.model.document.selection;
+
+        editor.model.change( writer => {
+            // Create a <placeholder> element with the "name" attribute (and all the selection attributes)...
+            const placeholder = writer.createElement( 'placeholder', {
+                ...Object.fromEntries( selection.getAttributes() ),
+                name: value
+            } );
+
+            // ... and insert it into the document. Put the selection on the inserted element.
+            editor.model.insertObject( placeholder, null, null, { setSelection: 'on' } );
+        } );
+    }
+
+    refresh() {
+        const model = this.editor.model;
+        const selection = model.document.selection;
+
+        const isAllowed = model.schema.checkChild( selection.focus.parent, 'placeholder' );
+
+        this.isEnabled = isAllowed;
+    }
+}
+
+class PlaceholderUI extends Plugin {
+    init() {
+        const editor = this.editor;
+        const t = editor.t;
+        const placeholderNames = editor.config.get( 'placeholderConfig.types' );
+
+        // The "placeholder" dropdown must be registered among the UI components of the editor
+        // to be displayed in the toolbar.
+        editor.ui.componentFactory.add( 'placeholder', locale => {
+            const dropdownView = createDropdown( locale );
+
+            // Populate the list in the dropdown with items.
+            addListToDropdown( dropdownView, getDropdownItemsDefinitions( placeholderNames ) );
+
+            dropdownView.buttonView.set( {
+                // The t() function helps localize the editor. All strings enclosed in t() can be
+                // translated and change when the language of the editor changes.
+                label: t( 'Rate Variables' ),
+                tooltip: true,
+                withText: true
+            } );
+
+            // Disable the placeholder button when the command is disabled.
+            const command = editor.commands.get( 'placeholder' );
+            dropdownView.bind( 'isEnabled' ).to( command );
+
+            // Execute the command when the dropdown item is clicked (executed).
+            this.listenTo( dropdownView, 'execute', evt => {
+                editor.execute( 'placeholder', { value: evt.source.commandParam } );
+                editor.editing.view.focus();
+            } );
+
+            return dropdownView;
+        } );
+    }
+}
+
+function getDropdownItemsDefinitions( placeholderNames ) {
+    const itemDefinitions = new Collection();
+
+    for ( const name of placeholderNames ) {
+        const definition = {
+            type: 'button',
+            model: new Model( {
+                commandParam: name,
+                label: name,
+                withText: true
+            } )
+        };
+
+        // Add the item definition to the collection.
+        itemDefinitions.add( definition );
+    }
+
+    return itemDefinitions;
+}
+
+class PlaceholderEditing extends Plugin {
+    static get requires() {
+        return [ Widget ];
+    }
+
+    init() {
+        console.log( 'PlaceholderEditing#init() got called' );
+
+        this._defineSchema();
+        this._defineConverters();
+
+        this.editor.commands.add( 'placeholder', new PlaceholderCommand( this.editor ) );
+
+        this.editor.editing.mapper.on(
+            'viewToModelPosition',
+            viewToModelPositionOutsideModelElement( this.editor.model, viewElement => viewElement.hasClass( 'placeholder' ) )
+        );
+        this.editor.config.define( 'placeholderConfig', {
+            types: [ 'rate', 'editdate' ]
+        } );
+    }
+
+    _defineSchema() {
+        const schema = this.editor.model.schema;
+
+        schema.register( 'placeholder', {
+            // Behaves like a self-contained inline object (e.g. an inline image)
+            // allowed in places where $text is allowed (e.g. in paragraphs).
+            // The inline widget can have the same attributes as text (for example linkHref, bold).
+            inheritAllFrom: '$inlineObject',
+
+            // The placeholder can have many types, like date, name, surname, etc:
+            allowAttributes: [ 'name' ]
+        } );
+    }
+
+    _defineConverters() {
+        const conversion = this.editor.conversion;
+
+        conversion.for( 'upcast' ).elementToElement( {
+            view: {
+                name: 'span',
+                classes: [ 'placeholder' ]
+            },
+            model: ( viewElement, { writer: modelWriter } ) => {
+                // Extract the "name" from "{name}".
+                const name = viewElement.getChild( 0 ).data.slice( 1, -1 );
+
+                return modelWriter.createElement( 'placeholder', { name } );
+            }
+        } );
+
+        conversion.for( 'editingDowncast' ).elementToElement( {
+            model: 'placeholder',
+            view: ( modelItem, { writer: viewWriter } ) => {
+                const widgetElement = createPlaceholderView( modelItem, viewWriter );
+
+                // Enable widget handling on a placeholder element inside the editing view.
+                return toWidget( widgetElement, viewWriter );
+            }
+        } );
+
+        conversion.for( 'dataDowncast' ).elementToElement( {
+            model: 'placeholder',
+            view: ( modelItem, { writer: viewWriter } ) => createPlaceholderView( modelItem, viewWriter )
+        } );
+
+        // Helper method for both downcast converters.
+        function createPlaceholderView( modelItem, viewWriter ) {
+            const name = modelItem.getAttribute( 'name' );
+
+            const placeholderView = viewWriter.createContainerElement( 'span', {
+                class: 'ratevar ' + name
+            } );
+
+            // Insert the placeholder name (as a text).
+            const innerText = viewWriter.createText( '{' + name + '}' );
+            viewWriter.insert( viewWriter.createPositionAt( placeholderView, 0 ), innerText );
+
+            return placeholderView;
+        }
+    }
+}
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        plugins: [ Essentials, Paragraph, Heading, List, Bold, Italic, Ratevars ],
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'numberedList', 'bulletedList', '|', 'placeholder' ],
+        placeholderConfig: {
+            types: [ 'rate', 'editdate' ]
+        }
+    } )
+    .then( editor => {
+        console.log( 'Editor was initialized', editor );
+
+        // Expose for playing in the console.
+        window.editor = editor;
+    } )
+    .catch( error => {
+        console.error( error.stack );
+    } );
